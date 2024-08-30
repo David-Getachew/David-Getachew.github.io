147abd8f39ea27b53f6e38ed78dfec299a10fd00
David-Getachew <dgetachew101@gmail.com>
2024-08-29 23:55:59 -0900

    Rebuilt the navbar and also edited the news section

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,3 +37,55 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el)=>observer.observe(el));
+
+// document.addEventListener("DOMContentLoaded", function(){
+//   // make it as accordion for smaller screens
+//   if (window.innerWidth > 992) {
+  
+//     document.querySelectorAll('.navbar .nav-item').forEach(function(everyitem){
+  
+//       everyitem.addEventListener('mouseover', function(e){
+  
+//         let el_link = this.querySelector('a[data-bs-toggle]');
+  
+//         if(el_link != null){
+//           let nextEl = el_link.nextElementSibling;
+//           el_link.classList.add('show');
+//           nextEl.classList.add('show');
+//         }
+  
+//       });
+//       everyitem.addEventListener('mouseleave', function(e){
+//         let el_link = this.querySelector('a[data-bs-toggle]');
+  
+//         if(el_link != null){
+//           let nextEl = el_link.nextElementSibling;
+//           el_link.classList.remove('show');
+//           nextEl.classList.remove('show');
+//         }
+  
+  
+//       })
+//     });
+  
+//   }
+//   // end if innerWidth
+//   }); 
+
+$(function() {
+
+  $('.custom-dropdown').on('show.bs.dropdown', function() {
+    // $(this).find('.dropdown-menu').first().stop(false, false).slideDown();
+     // $(this).find('.dropdown-menu').addClass('active');
+     var that = $(this);
+    setTimeout(function(){
+        that.find('.dropdown-menu').addClass('active');
+    }, 100);
+    
+
+  });
+  $('.custom-dropdown').on('hide.bs.dropdown', function() {
+    $(this).find('.dropdown-menu').removeClass('active');
+  });
+
+});
